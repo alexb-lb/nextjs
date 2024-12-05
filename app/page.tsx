980c0dca512ebaa99c9964668f3a2dee6be2a38d
Alex Bobrenko <oknerbob@gmail.com>
2024-12-05 13:25:51 +0100

    update inject cookies file

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Script from "next/script";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Script src={"/injectCookies.js"} type="text/javascript" />
+      <Script src={"./injectCookies.js"} type="text/javascript" />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
