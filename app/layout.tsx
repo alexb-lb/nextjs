e663aa6478fd68d51d215b6e76a823e0024becd0
Alex Bobrenko <oknerbob@gmail.com>
2024-12-05 12:20:01 +0100

    update ga tag manager code. Enable alert

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,12 +33,32 @@ export default function RootLayout({
           id="lb-cookie-consent"
           strategy="beforeInteractive"
         ></Script> */}
-        {/* <Script src="https://test-cookies.tiiny.site/show_alert.js" strategy="beforeInteractive" /> */}
-        <GoogleTagManager gtmId="GTM-5GXBGFPT" />
+        <Script
+          src="https://test-cookies.tiiny.site/show_alert.js"
+          strategy="beforeInteractive"
+        />
+        <Script id="google-tag-manager">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5GXBGFPT');
+          `}
+        </Script>
+        {/* <GoogleTagManager gtmId="GTM-5GXBGFPT" /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5GXBGFPT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         {children}
       </body>
     </html>
