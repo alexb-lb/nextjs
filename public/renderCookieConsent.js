2e0b20c7109083ea91f7a08f2e1c8d2df62e950a
Alex Bobrenko <oknerbob@gmail.com>
2024-12-05 13:46:44 +0100

    inject cookies from vercel site itself

--- public/renderCookieConsent.js
+++ public/renderCookieConsent.js
@@ -1155,6 +1155,8 @@ var renderCookieConsent = async () => {
     "^/",
     "^./",
     window.location.host,
+    getLbMainDomain(),
+    "." + getLbMainDomain(),
     ...(s3DomainName ? [s3DomainName] : []),
     ...(webAppDomainName ? [webAppDomainName] : []),
   ];
