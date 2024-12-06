3bdcc598af15408e0c19d27fb7477f2658b87b16
Piyush Goyal <72322419+piyush-lb@users.noreply.github.com>
2024-12-06 22:39:41 +0100

    update mainjs

--- public/main.js
+++ public/main.js
@@ -6,7 +6,6 @@ Object.defineProperty(document, 'cookie', {
         const cookieParts = value.split(';')[0].split('=');
         const cookieName = cookieParts[0].trim();
         const cookieValue = cookieParts[1]?.trim();
-        console.log(`Cookie set: ${cookieName}-${cookieValue}`);
         // Capture and parse the stack trace
         const stack = new Error().stack;
 
