ab3c6ca5ebe775a5526772e83f0650b06cb6437d
Alex Bobrenko <oknerbob@gmail.com>
2024-12-06 17:49:26 +0100

    use local files

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          // src="/main.js"
-          src="https://newcookiebucket.s3.us-east-2.amazonaws.com/cookie_consent_10/363cc9bc-7ee8-43de-bd64-4238ee416ba2/main_4f8046e.js"
+          src="/main.js"
+          // src="https://newcookiebucket.s3.us-east-2.amazonaws.com/cookie_consent_10/363cc9bc-7ee8-43de-bd64-4238ee416ba2/main_4f8046e.js"
           id="lb-cookie-consent"
           strategy="beforeInteractive"
         ></Script>
