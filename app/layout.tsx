23f3663b548930fe11222a2a48fa86c79179cb48
Alex Bobrenko <oknerbob@gmail.com>
2025-02-04 11:34:26 +0100

    test block/unblock gtag scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
+          src="/main.js?gcm=true&prefCenterOnly=false"
+          // src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_44/8f3da2ac-7052-4995-bc73-1487cab95165/main_dd201b6.js?gcm=true&prefCenterOnly=false"
+          id="lb-cookie-consent"
+          data-gcm="true"
+          strategy="beforeInteractive"
+        />
+        <Script
           id="gtag-default"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
@@ -53,13 +60,6 @@ export default function RootLayout({
           }}
         />
         <GoogleTagManager gtmId={GTM_ID} />
-        <Script
-          src="/main.js?gcm=true&prefCenterOnly=false"
-          // src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_44/8f3da2ac-7052-4995-bc73-1487cab95165/main_dd201b6.js?gcm=true&prefCenterOnly=false"
-          id="lb-cookie-consent"
-          data-gcm="true"
-          strategy="beforeInteractive"
-        />
         <Script
           src="./injectCookies.js"
           type="text/javascript"
