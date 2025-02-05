248d6466d9879f2c8f0fb1d21c19e1f978ed0ee8
Alex Bobrenko <oknerbob@gmail.com>
2025-02-05 12:49:19 +0100

    gcm=on, external script

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <Script
+        <Script
           id="gtag-default"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
@@ -51,12 +51,10 @@ export default function RootLayout({
               });
             `,
           }}
-        /> */}
+        />
         <Script
           // src="/main.js?gcm=true&prefCenterOnly=false"
-          // src="https://sahil-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_15/408ecbd2-0ae8-4be8-9dc7-1da9c11abb89/main_5fd21c3.js?gcm=false&prefCenterOnly=false"
-          // id="lb-cookie-consent"
-          src="https://sahil-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_15/408ecbd2-0ae8-4be8-9dc7-1da9c11abb89/main_5fd21c3.js?gcm=false&prefCenterOnly=false" 
+          src="https://sahil-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_17/408ecbd2-0ae8-4be8-9dc7-1da9c11abb89/main_5d20284.js?gcm=true&prefCenterOnly=false" 
           id="lb-cookie-consent"
           strategy="beforeInteractive"
         />
