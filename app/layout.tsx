399a843cfcbdc676077258eccc80327d14e589d4
Alex Bobrenko <oknerbob@gmail.com>
2025-02-10 16:42:42 +0100

    change version to 53

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
         <GoogleTagManager gtmId={GTM_ID} />
         <Script
           // src="/main.js?gcm=true&prefCenterOnly=false"
-          src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_51/4a56b734-e393-4bc2-9f17-a401c9bef803/main_01c4ea5.js?gcm=true&prefCenterOnly=false"
+          src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_53/4a56b734-e393-4bc2-9f17-a401c9bef803/main_3f98e8a.js?gcm=true&prefCenterOnly=false"
           id="lb-cookie-consent"
           strategy="beforeInteractive"
         />
