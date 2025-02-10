eca36571d6d4ad899f2e42bad72fbbd0f15ecf2d
Alex Bobrenko <oknerbob@gmail.com>
2025-02-10 16:18:00 +0100

    update cookie script

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
         <GoogleTagManager gtmId={GTM_ID} />
         <Script
           // src="/main.js?gcm=true&prefCenterOnly=false"
-          src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_48/8f3da2ac-7052-4995-bc73-1487cab95165/main_9db21ec.js?gcm=true&prefCenterOnly=false"
+          src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_49/8f3da2ac-7052-4995-bc73-1487cab95165/main_cc9a38e.js?gcm=true&prefCenterOnly=false"
           id="lb-cookie-consent"
           strategy="beforeInteractive"
         />
