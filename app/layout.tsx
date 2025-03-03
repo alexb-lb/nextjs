840c6a7ca428c57c966e1f1067f0fd4442538e38
Alex Bobrenko <oknerbob@gmail.com>
2025-03-03 09:20:54 +0100

    use script from gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <Script
+        <Script
           id="gtag-default"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
@@ -51,11 +51,12 @@ export default function RootLayout({
               });
             `,
           }}
-        /> */}
+        />
         <GoogleTagManager gtmId={GTM_ID} />
         {/* <Script
           // src="/main.js?gcm=true&prefCenterOnly=false"
-          src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_58/4a56b734-e393-4bc2-9f17-a401c9bef803/main_972f0e1.js?gcm=true&prefCenterOnly=false"
+          src="https://lb-cctest.s3.ap-south-1.amazonaws.com/cookie_consent_58/4a56b734-e393-4bc2-9f17-a401c9bef803/main_972f0e1.js
+          ?gcm=true&prefCenterOnly=false&defaultConsent=acceptAll"
           id="lb-cookie-consent"
           strategy="beforeInteractive"
         /> */}
